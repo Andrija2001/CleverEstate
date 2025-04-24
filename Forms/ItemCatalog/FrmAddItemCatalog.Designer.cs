9d1443f73c6966741b0e4c328e61493449b8e707
Andrija <86878450+Andrija2001@users.noreply.github.com>
2025-04-24 13:17:45 +0200

    Update FrmAddItemCatalog.Designer.cs

--- Forms/ItemCatalog/FrmAddItemCatalog.Designer.cs
+++ Forms/ItemCatalog/FrmAddItemCatalog.Designer.cs
@@ -138,6 +138,7 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "Add Catalog Item";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // FrmAddItemCatalog
             // 
