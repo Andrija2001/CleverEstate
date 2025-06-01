83a07da2390b3e76ef1893b85096b6daf2043472
Andrija <86878450+Andrija2001@users.noreply.github.com>
2025-06-02 00:51:42 +0200

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,7 +34,17 @@ namespace CleverEstate
             this.ApartmantsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.InvoiceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ItemCatalogToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grupniRačunToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.klijentiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -45,7 +55,7 @@ namespace CleverEstate
             this.glavniMeniToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(844, 29);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -55,62 +65,190 @@ namespace CleverEstate
             this.BuildingsToolStripMenuItem,
             this.ApartmantsToolStripMenuItem,
             this.InvoiceToolStripMenuItem,
-            this.ItemCatalogToolStripMenuItem});
+            this.ItemCatalogToolStripMenuItem,
+            this.grupniRačunToolStripMenuItem,
+            this.klijentiToolStripMenuItem});
+            this.glavniMeniToolStripMenuItem.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.glavniMeniToolStripMenuItem.Name = "glavniMeniToolStripMenuItem";
-            this.glavniMeniToolStripMenuItem.Size = new System.Drawing.Size(82, 20);
+            this.glavniMeniToolStripMenuItem.Size = new System.Drawing.Size(112, 25);
             this.glavniMeniToolStripMenuItem.Text = "Glavni Meni";
             // 
             // BuildingsToolStripMenuItem
             // 
             this.BuildingsToolStripMenuItem.Name = "BuildingsToolStripMenuItem";
-            this.BuildingsToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
-            this.BuildingsToolStripMenuItem.Text = "Buildings";
+            this.BuildingsToolStripMenuItem.Size = new System.Drawing.Size(184, 26);
+            this.BuildingsToolStripMenuItem.Text = "Zgrade";
             this.BuildingsToolStripMenuItem.Click += new System.EventHandler(this.BuildingToolStripMenuItem_Click);
             // 
             // ApartmantsToolStripMenuItem
             // 
             this.ApartmantsToolStripMenuItem.Name = "ApartmantsToolStripMenuItem";
-            this.ApartmantsToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
-            this.ApartmantsToolStripMenuItem.Text = "Apartmans";
+            this.ApartmantsToolStripMenuItem.Size = new System.Drawing.Size(184, 26);
+            this.ApartmantsToolStripMenuItem.Text = "Apartmani";
             this.ApartmantsToolStripMenuItem.Click += new System.EventHandler(this.ApartmantsToolStripMenuItem_Click);
             // 
             // InvoiceToolStripMenuItem
             // 
             this.InvoiceToolStripMenuItem.Name = "InvoiceToolStripMenuItem";
-            this.InvoiceToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
-            this.InvoiceToolStripMenuItem.Text = "Invoice";
+            this.InvoiceToolStripMenuItem.Size = new System.Drawing.Size(184, 26);
+            this.InvoiceToolStripMenuItem.Text = "Računi";
             this.InvoiceToolStripMenuItem.Click += new System.EventHandler(this.InvoiceToolStripMenuItem_Click);
             // 
             // ItemCatalogToolStripMenuItem
             // 
             this.ItemCatalogToolStripMenuItem.Name = "ItemCatalogToolStripMenuItem";
-            this.ItemCatalogToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
-            this.ItemCatalogToolStripMenuItem.Text = "Item Catalog";
+            this.ItemCatalogToolStripMenuItem.Size = new System.Drawing.Size(184, 26);
+            this.ItemCatalogToolStripMenuItem.Text = "Stavke računa";
             this.ItemCatalogToolStripMenuItem.Click += new System.EventHandler(this.ItemCatalogToolStripMenuItem_Click);
+            // 
+            // grupniRačunToolStripMenuItem
+            // 
+            this.grupniRačunToolStripMenuItem.Name = "grupniRačunToolStripMenuItem";
+            this.grupniRačunToolStripMenuItem.Size = new System.Drawing.Size(184, 26);
+            this.grupniRačunToolStripMenuItem.Text = "Grupni račun";
+            this.grupniRačunToolStripMenuItem.Click += new System.EventHandler(this.grupniRačunToolStripMenuItem_Click);
             // 
             // dataGridView1
             // 
-            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(33, 46);
+            this.dataGridView1.Location = new System.Drawing.Point(19, 117);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(10);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(708, 367);
+            this.dataGridView1.Size = new System.Drawing.Size(806, 569);
             this.dataGridView1.TabIndex = 1;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
             this.dataGridView1.DataBindingComplete += new System.Windows.Forms.DataGridViewBindingCompleteEventHandler(this.dataGridView1_DataBindingComplete);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSearch.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(19, 75);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(5);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(133, 29);
+            this.txtSearch.TabIndex = 2;
+            this.txtSearch.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 44);
+            this.label1.Margin = new System.Windows.Forms.Padding(5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Broj računa";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.DropDownWidth = 120;
+            this.comboBox1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Plaćeni",
+            "Neplaćeni",
+            "Svi računi"});
+            this.comboBox1.Location = new System.Drawing.Point(231, 78);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(10);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(133, 29);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.dateTimePicker1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(473, 78);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(133, 29);
+            this.dateTimePicker1.TabIndex = 5;
+            this.dateTimePicker1.CloseUp += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(469, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 21);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Od:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.dateTimePicker2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker2.Location = new System.Drawing.Point(681, 78);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(103, 29);
+            this.dateTimePicker2.TabIndex = 7;
+            this.dateTimePicker2.CloseUp += new System.EventHandler(this.dateTimePicker2_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(677, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 21);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Do";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(227, 42);
+            this.label4.Margin = new System.Windows.Forms.Padding(10);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(121, 21);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Status plaćanja";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // klijentiToolStripMenuItem
+            // 
+            this.klijentiToolStripMenuItem.Name = "klijentiToolStripMenuItem";
+            this.klijentiToolStripMenuItem.Size = new System.Drawing.Size(184, 26);
+            this.klijentiToolStripMenuItem.Text = "Klijenti";
+            this.klijentiToolStripMenuItem.Click += new System.EventHandler(this.klijentiToolStripMenuItem_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(844, 693);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtSearch);
             this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.comboBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Broj računa";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
@@ -129,6 +267,16 @@ namespace CleverEstate
         private System.Windows.Forms.ToolStripMenuItem InvoiceToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem ItemCatalogToolStripMenuItem;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ToolStripMenuItem grupniRačunToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem klijentiToolStripMenuItem;
     }
 }
 
