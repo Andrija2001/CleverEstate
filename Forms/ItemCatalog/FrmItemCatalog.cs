c7ce657f7bb4191c02300ff9935b530abe9c1ed8
Andrija <86878450+Andrija2001@users.noreply.github.com>
2025-06-02 00:46:29 +0200

    Update FrmItemCatalog.cs

--- Forms/ItemCatalog/FrmItemCatalog.cs
+++ Forms/ItemCatalog/FrmItemCatalog.cs
@@ -1,115 +1,88 @@
+using CleverEstate.Forms.Clients;
 using CleverEstate.Models;
+using CleverEstate.Services.Classes;
 using CleverEstate.Services.Classes.Repository;
+using CleverEstate.Services.Interface;
 using CleverState.Services.Classes;
 using System;
-using System.Drawing;
 using System.Windows.Forms;
-
 namespace CleverEstate.Forms.CatalogItem
 {
-    public partial class FrmItemCatalog : Form
+    public partial class FrmAddItemCatalog : Form
     {
-    
-        public BindingSource bindingSource1 = new BindingSource();
-        private ItemCatalogService service;
-        private ItemCatalogRepository repository;
-        Font font = new Font("Arial", 12);
-        public FrmItemCatalog()
+        private ItemCatalogRepository _repository;
+        FrmItemCatalog FrmItemCatalog;
+        private ItemCatalog currentCatalogItem;
+        private bool isEditMode;
+        public FrmAddItemCatalog(FrmItemCatalog parentForm, ItemCatalogRepository itemCatalogRepository, ItemCatalog CatalogItemToEdit)
+        : this(parentForm, itemCatalogRepository)
         {
-            repository = new ItemCatalogRepository(new DataDbContext());
+            this.currentCatalogItem = CatalogItemToEdit;
+            this.isEditMode = true;
+            this.Text = "FrmEditClients";
+            button1.Text =  "OK";
+            txtName.Text = CatalogItemToEdit.Name;
+            txtPricePerUnit.Text = CatalogItemToEdit.PricePerUnit.ToString();
+            comboBox1.SelectedItem = CatalogItemToEdit.Unit;
+        }
+        public FrmAddItemCatalog(FrmItemCatalog frmItemCatalog, ItemCatalogRepository itemCatalogRepository)
+        {
             InitializeComponent();
-            bindingSource1.DataSource = typeof(ItemCatalog);
+            this.FrmItemCatalog = frmItemCatalog;
+            this._repository = itemCatalogRepository;
+            _repository = new ItemCatalogRepository(new DataDbContext());
 
         }
-
-
-        private void FrmItemCatalog_Load(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-            LoadCatalogItems();
-        }
-
-        private void LoadCatalogItems()
-        {
-            var listItemCatalogs = repository.GetAll();
-            bindingSource1.DataSource = listItemCatalogs;
-            dataGridView1.DataSource = bindingSource1;
-        }
-
-        private void addNewRowButton_Click(object sender, EventArgs e)
-        {
-            FrmAddItemCatalog frmAddItemCatalog = new FrmAddItemCatalog(this, repository);
-            frmAddItemCatalog.ShowDialog();
-        }
-        private void dataGridView1_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
-        {
-            if (!dataGridView1.Columns.Contains("Edit"))
+            if (!isEditMode)
             {
-                var btnEdit = new DataGridViewButtonColumn
+                ItemCatalog itemCatalog = new ItemCatalog();
+                if  (txtName.Text == "" || txtPricePerUnit.Text == "")
                 {
-                    Name = "Edit",
-                    Text = "Edit",
-                    UseColumnTextForButtonValue = true
-                };
-                dataGridView1.Columns.Add(btnEdit);
-            }
-            dataGridView1.Columns["Edit"].HeaderText = "";
-            if (dataGridView1.Columns.Count > 2)
-            {
-                dataGridView1.Columns["Edit"].DisplayIndex = 4;
-            }
-            if (!dataGridView1.Columns.Contains("Delete"))
-            {
-                var btnDelete = new DataGridViewButtonColumn
-                {
-                    Name = "Delete",
-                    Text = "Delete",
-                    UseColumnTextForButtonValue = true,
-                };
-                dataGridView1.Columns.Add(btnDelete);
-            }
-            dataGridView1.Columns["Delete"].HeaderText = "";
-            if (dataGridView1.Columns.Count > 3)
-            {
-                dataGridView1.Columns["Delete"].DisplayIndex = 5;
-            }
-            if (dataGridView1.Columns.Contains("Id"))
-            {
-                dataGridView1.Columns["Id"].Visible = false;
-            }
-        }
-        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
-        {
-            if (e.RowIndex < 0 || e.ColumnIndex < 0)
-                return;
-
-            if (dataGridView1.Columns[e.ColumnIndex].Name == "Delete")
-            {
-                var buildingToDelete = (Guid)dataGridView1.Rows[e.RowIndex].Cells["Id"].Value;
-                repository.Delete(buildingToDelete); 
-                BindingSource bindingSource = (BindingSource)dataGridView1.DataSource;
-                bindingSource.RemoveAt(e.RowIndex);
-                dataGridView1.Refresh();
-            }
-            if (dataGridView1.Columns[e.ColumnIndex].Name == "Edit")
-                {
-                    var selectedCatalogItem = (ItemCatalog)dataGridView1.Rows[e.RowIndex].DataBoundItem;
-                    FrmAddItemCatalog frm3 = new FrmAddItemCatalog(this, repository, selectedCatalogItem);
-                    frm3.ShowDialog();
-                    int index = bindingSource1.IndexOf(selectedCatalogItem);
-                    if (index != -1)
-                    {
-                        var updatedItemCatalog = new ItemCatalog
-                        {
-                            Id = selectedCatalogItem.Id,
-                            Name = selectedCatalogItem.Name,
-                            PricePerUnit = selectedCatalogItem.PricePerUnit,
-                            Unit = selectedCatalogItem.Unit,
-                        };
-                        repository.Update(updatedItemCatalog);
-                        bindingSource1[index] = updatedItemCatalog;
-                        bindingSource1.ResetBindings(false);
-                    }
+                    return;
                 }
+                string name = txtName.Text;
+                decimal pricePerUnit = decimal.Parse(txtPricePerUnit.Text);
+                string unit = comboBox1.SelectedItem.ToString();
+                itemCatalog.Id = Guid.NewGuid();
+                itemCatalog.Name = name;
+                itemCatalog.PricePerUnit = pricePerUnit;
+                itemCatalog.Unit = unit;
+                _repository.Insert(itemCatalog);
+                FrmItemCatalog.bindingSource1.Add(itemCatalog);
+            }
+            else
+            {
+                currentCatalogItem.Name = txtName.Text;
+                currentCatalogItem.PricePerUnit = decimal.Parse(txtPricePerUnit.Text);
+                currentCatalogItem.Unit = comboBox1.SelectedItem.ToString();    
+                _repository.Update(currentCatalogItem);
+                this.Close();
+            }
+        }
+        private void txtUnit_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                 (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+        private void txtName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Back)
+            {
+                return;
+            }
+            if (!Char.IsLetter(e.KeyChar) && !Char.IsWhiteSpace(e.KeyChar))
+            {
+                e.Handled = true;
             }
         }
     }
+}
